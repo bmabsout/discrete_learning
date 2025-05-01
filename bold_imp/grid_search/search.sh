bc9f2ab436be7f3db155e81b6ceba966e28d14f4
Weifan <chenweifan9@gmail.com>
2025-05-01 15:30:00 -0400

    auto sync

--- bold_imp/grid_search/search.sh
+++ bold_imp/grid_search/search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ../../../ml2/venv/bin/activate
+source /usr3/graduate/wfchen/ml2/venv/bin/activate
 
 cd ..
 
@@ -30,6 +30,8 @@ flip_epochs=("1" "5" "10")
 flip_baas=("0.1" "0.3" "0.6" "0.9")
 batch_sizes=("64" "128" "256" "350")
 
+id=0
+
 # Loop through all combinations of parameters
 for l1_scale in "${l1_scales[@]}"; do
     for l1_salient in "${l1_salients[@]}"; do
@@ -38,6 +40,8 @@ for l1_scale in "${l1_scales[@]}"; do
                 for flip_epoch in "${flip_epochs[@]}"; do
                     for flip_baa in "${flip_baas[@]}"; do
                         for batch_size in "${batch_sizes[@]}"; do
+                            echo "(ID=$id) Running with parameters: l1_scale=$l1_scale, l1_salient=$l1_salient, act_range=$act_range, flip_ratio=$flip_ratio, flip_epoch=$flip_epoch, flip_baa=$flip_baa, batch_size=$batch_size"
+
                             python3 bold_int.py --dataset cifar10 --arch-custom --opt-momentum --opt-probabilistic --input-int --loss-int-l1 \
                             --spread-std \
                             --epochs 10 \
@@ -49,6 +53,9 @@ for l1_scale in "${l1_scales[@]}"; do
                             --prob-flip-ratio-batch-acc-aware $flip_baa \
                             --batch-size $batch_size \
                             --arch test
+
+                            echo "(ID=$id) Finished!"
+                            id=$((id+1))
                         done
                     done               
                 done
